6bd47a704641cd31b4ac69902c92b5becb2f6a6e
Damian Adams <contact@mianlabs.com>
2023-04-07 17:17:48 -0500

    Add Vuetify 3

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,25 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// Vuetify
+import webFontLoader from 'webfontloader'
+
+webFontLoader.load({
+  google: {
+    families: ['Roboto:100,300,400,500,700,900&display=swap']
+  }
+})
+
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+const vuetify = createVuetify()
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
