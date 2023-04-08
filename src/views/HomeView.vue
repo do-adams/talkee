831cfb3d03a9bc8567cc8c52026dc999e9dbe9f9
Damian Adams <contact@mianlabs.com>
2023-04-08 18:08:32 -0500

    Update home view with auth state

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,23 @@
 <template>
-  <main>Hello World!</main>
+  <h1>Home View</h1>
+  <p>Is signed in? {{ isAuthenticated }}</p>
+  <p>
+    <code>
+      {{ user }}
+    </code>
+  </p>
+  <button v-if="isAuthenticated" @click="onSignOut">Sign Out</button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useAuthStore } from '@/stores/auth'
+import { getAuth, signOut } from 'firebase/auth'
+
+const { user, isAuthenticated } = storeToRefs(useAuthStore())
+const auth = getAuth()
+
+const onSignOut = () => {
+  signOut(auth)
+}
+</script>
