6fde752791631001b6c55212e94df4d11d418426
Damian Adams <contact@mianlabs.com>
2023-04-08 18:08:20 -0500

    Create auth store

---
+++ src/stores/auth.ts
@@ -0,0 +1,24 @@
+import { getAuth, onAuthStateChanged, type User } from 'firebase/auth'
+import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
+
+export const useAuthStore = defineStore('auth', () => {
+  const auth = getAuth()
+
+  const user = ref<User | null | undefined>(undefined)
+  const isAuthenticated = computed(() => !!user.value)
+
+  const setUser = async (newUser: User | null) => {
+    user.value = newUser
+  }
+
+  const init = new Promise<void>((resolve) =>
+    onAuthStateChanged(auth, (newUser) => setUser(newUser).finally(resolve))
+  )
+
+  return {
+    user,
+    isAuthenticated,
+    isReady: init
+  }
+})
