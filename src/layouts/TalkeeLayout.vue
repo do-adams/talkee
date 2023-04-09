d052938328a2d498451fa0eddb3459512bce41f3
Damian Adams <contact@mianlabs.com>
2023-04-09 16:09:18 -0500

    Clean up the layout

--- src/layouts/TalkeeLayout.vue
+++ src/layouts/TalkeeLayout.vue
@@ -16,20 +16,20 @@
       </v-container>
     </v-app-bar>
 
-    <v-main class="bg-grey-lighten-3">
+    <v-main>
       <v-container>
         <v-row>
           <v-col cols="2">
             <v-sheet rounded="lg">
               <v-list rounded="lg">
                 <v-list-item v-for="n in 5" :key="n" link>
-                  <v-list-item-title> List Item {{ n }} </v-list-item-title>
+                  <!-- <v-list-item-title> List Item {{ n }} </v-list-item-title> -->
                 </v-list-item>
 
                 <v-divider class="my-2"></v-divider>
 
                 <v-list-item link color="grey-lighten-4">
-                  <v-list-item-title> Refresh </v-list-item-title>
+                  <!-- <v-list-item-title> Refresh </v-list-item-title> -->
                 </v-list-item>
               </v-list>
             </v-sheet>
