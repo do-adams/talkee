980faa5d93952a7d312b67e93156a797b45fe112
Damian Adams <contact@mianlabs.com>
2023-04-09 21:31:19 -0500

    Add Vuetify-compatible type annotations for validators

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,22 +1,30 @@
 import { string, type ValidationError } from 'yup'
 
-export const requiredRule =
+type Result = string | boolean
+
+type Validator<T, U> = (
+  options?: T
+) => Result | ((value: U) => Result) | ((value: U) => PromiseLike<Result>)
+
+export const requiredRule: Validator<string, unknown> =
   (message = 'This field is required') =>
-  <T>(value: T) =>
+  (value) =>
     Boolean(value) || message
 
-export const emailRule =
+export const emailRule: Validator<string, string> =
   (message = 'Please enter a valid email') =>
-  (value: string) =>
+  (value) =>
     string()
       .email(message)
       .validate(value)
       .then(() => true)
       .catch((e: ValidationError) => e.errors[0])
 
-export const minLengthRule = (n: number) => (value: string) =>
-  string()
-    .min(n, `This field must be at least ${n} characters long`)
-    .validate(value)
-    .then(() => true)
-    .catch((e: ValidationError) => e.errors[0])
+export const minLengthRule: Validator<number, string> =
+  (n = 5) =>
+  (value) =>
+    string()
+      .min(n, `This field must be at least ${n} characters long`)
+      .validate(value)
+      .then(() => true)
+      .catch((e: ValidationError) => e.errors[0])
