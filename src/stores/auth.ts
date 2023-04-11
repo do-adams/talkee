1f68f29cfa5cf5d055ab24986280249b13ae4338
Damian Adams <contact@mianlabs.com>
2023-04-11 01:37:54 -0500

    Update store types

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -5,12 +5,10 @@ import { computed, ref } from 'vue'
 export const useAuthStore = defineStore('auth', () => {
   const auth = getAuth()
 
-  const user = ref<User | null | undefined>(undefined)
+  const user = ref<User | null>(null)
   const isAuthenticated = computed(() => !!user.value)
 
-  const setUser = async (newUser: User | null) => {
-    user.value = newUser
-  }
+  const setUser = async (newUser: User | null) => (user.value = newUser)
 
   const init = new Promise<void>((resolve) =>
     onAuthStateChanged(auth, (newUser) => setUser(newUser).finally(resolve))
