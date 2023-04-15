f43f3460e0dc0e1e4f5b900308bcdd2404992a3f
Damian Adams <contact@mianlabs.com>
2023-04-14 22:24:08 -0500

    Expand options for testing library custom renderer

--- src/vitest/testing-library.ts
+++ src/vitest/testing-library.ts
@@ -1,20 +1,34 @@
-import { render } from '@testing-library/vue'
+import { render, type RenderOptions } from '@testing-library/vue'
 
 import { createPinia } from 'pinia'
-import { createVuetify } from 'vuetify'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createVuetify, type VuetifyOptions } from 'vuetify'
+import { createRouter, createWebHistory, type RouterOptions } from 'vue-router'
 
-const customRender: typeof render = (ui, options) =>
+interface CustomOptions {
+  render?: RenderOptions
+  router?: RouterOptions
+  vuetify?: VuetifyOptions
+}
+
+const defaultOptions: CustomOptions = {
+  router: { history: createWebHistory(), routes: [] },
+  vuetify: {}
+}
+
+const customRender = (ui: unknown, options: CustomOptions = {}) => {
+  options = { ...defaultOptions, ...options }
+
   render(ui, {
-    ...options,
+    ...options.render,
     global: {
       plugins: [
         createPinia(),
-        createRouter({ history: createWebHistory(), routes: [] }),
-        createVuetify()
+        createRouter(options.router as RouterOptions),
+        createVuetify(options.vuetify)
       ]
     }
   })
+}
 
 // re-export everything
 export * from '@testing-library/vue'
