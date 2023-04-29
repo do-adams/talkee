5b76f50b7419c7a8bc66105cc5cc1cce65e00909
Damian Adams <contact@mianlabs.com>
2023-04-28 22:41:48 -0500

    Add global error snackbar markup and composable

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,19 @@
       <talkee-layout>
         <router-view />
       </talkee-layout>
+      <v-snackbar v-model="showSnackbar">
+        {{ message }}
+        <template #actions>
+          <v-btn color="pink" variant="text" @click="showSnackbar = false">Close</v-btn>
+        </template>
+      </v-snackbar>
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
 import TalkeeLayout from '@/layouts/TalkeeLayout.vue'
+import { useErrorSnackbar } from '@/composables/useErrorSnackbar'
+
+const { message, showSnackbar } = useErrorSnackbar()
 </script>
